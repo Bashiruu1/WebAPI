cc6652734696af01ffac9e7663f06fc9ad899797
Bashiruu1 <silvergold21@gmail.com>
2020-07-17 23:20:54 -0400

    Initial commit

---
+++ Services/GitHubService/IGitHubService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Services.GitHubService
+{
+    public interface IGitHubService
+    {
+        Task<string> GetReprositories(string User);
+    }
+}
